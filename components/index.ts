e6bfe70b55c1aa0e4c31fa33d2ed7450a31c2570
abedsully <abedw2909@gmail.com>
2023-07-22 18:54:26 +0700

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from './Hero';
+import CustomButton  from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Searchbar from './Searchbar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import Carcard from './Carcard';
+import CarDetails from './Cardetails';
+import Showmore from './Showmore';
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    Searchbar,
+    CustomFilter,
+    SearchManufacturer,
+    Carcard,
+    CarDetails,
+    Showmore
+}
